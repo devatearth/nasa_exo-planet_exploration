95d185cab15d7b3f9f58649fcc3237bf4198022d
devatearth <64774809+devatearth@users.noreply.github.com>
2023-05-07 18:38:41 +0530

    Code changes for get all planets and onload setup

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,3 +1,30 @@
-const planets = ["kepler-403"];
+const path = require('path');
+const { parse } = require('csv-parse');
+const fs = require('fs');
 
-module.exports = planets;
+const habitablePlanet = [];
+
+function isHabitablePlanet(planet) {
+    return planet['koi_disposition'] === 'CONFIRMED'
+        && planet['koi_insol'] > 0.36
+        && planet['koi_insol'] < 1.11
+        && planet['koi_prad'] < 1.6;
+}
+const loadPlanetsData = new Promise((resolve, reject) => {
+    fs.createReadStream(path.join(__dirname, '..', 'data', 'Kepler_data.csv'))
+        .pipe(parse({
+            comment: '#',
+            columns: true,
+        }))
+        .on('data', (data) => {
+            if (isHabitablePlanet(data))
+                habitablePlanet.push(data);
+        })
+        .on('end', () => {
+            console.log(`${habitablePlanet.length} habitable planets found!`);
+            resolve();
+        });
+});
+
+
+module.exports = { planets: habitablePlanet, loadPlanetsData };
